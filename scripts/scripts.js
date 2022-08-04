dcc1d734a30f1f2d1daf408136adce08ba4615ad
Eugene Starodubov <starodubov.eugenij@yandex.ru>
2022-08-05 01:37:26 +0400

    feat JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,33 @@
 let container = document.querySelector(`.popup`);
 let editButton = document.querySelector(`.profile__edit-button`);
 let closeButton = container.querySelector(`.popup__close-button`);
-let checkPopupOpened = container.classList.contains(`.popup_opened`);
 
-editButton.addEventListener(`click`, () =>
-	container.classList.add(`popup_opened`)
-);
-
-closeButton.addEventListener(`click`, () =>
-	container.classList.remove(`popup_opened`)
-); /*
-
-closeButton.addEventListener(`click`, closePopup);
+editButton.addEventListener(`click`, showPopup);
+closeButton.addEventListener(`click`, hidePopup);
 
 function showPopup() {
-	checkWindow = container.classList.add(`popup_opened`);
+	container.classList.add(`popup_opened`);
 }
 
-function closePopup() {
-	checkWindow = container.classList.remove(`popup_opened`);
+function hidePopup() {
+	container.classList.remove(`popup_opened`);
 }
 
-checkWindow();
-*/
+let profileName = document.querySelector(`.profile__title`);
+let profileJob = document.querySelector(`.profile__subtitle`);
+let saveButton = document.querySelector(`.popup__button`);
+
+//saveButton.addEventListener(`submit`, newUser);
+
+let nameInput = document.querySelector(`.popup__input-name`);
+let jobInput = document.querySelector(`.popup__input-job`);
+let popupForm = document.querySelector(`.popup__container`);
+
+function formSubmitHandler(evt) {
+	evt.preventDefault();
+	profileName.textContent = nameInput.value;
+	profileJob.textContent = jobInput.value;
+	hidePopup();
+}
+
+popupForm.addEventListener(`submit`, formSubmitHandler);
